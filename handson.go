07d7e34d324ac8aa55f200275c97bff03ed9faf5
高島恭平 <kyohei_takashima@kyomac.local>
2025-08-08 15:26:31 +0900

    if-boolのハンズオン追加

--- handson.go
+++ handson.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 func HandsonRoutes() {
@@ -11,6 +12,23 @@ func HandsonRoutes() {
 			fmt.Fprintf(w, "Line %d\n", i)
 		}
 	})
+
+	http.HandleFunc("/if-bool", func(w http.ResponseWriter, r *http.Request) {
+		// クエリパラメータから値を取��
+		valueStr := r.URL.Query().Get("value")
+
+		// 文字列を整数に変換
+		value, err := strconv.Atoi(valueStr)
+
+		if err != nil {
+			fmt.Fprintf(w, "エラー: 整数値を入力してください\n")
+			return
+		}
+
+		if value > 10 {
+			fmt.Fprintf(w, "入力された値は10より大きいです: %d\n", value)
+		} else {
+			fmt.Fprintf(w, "入力された値は10以下です: %d\n", value)
+		}
+	})
 }
-
-
